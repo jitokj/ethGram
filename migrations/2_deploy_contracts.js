b1f8c19c0ea9f8801c478d04438355640533e19e
jito <jitokj3@gmail.com>
2021-04-29 15:28:30 +0530

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Ethtragram = artifacts.require("Ethgram");
+
+module.exports = function (deployer) {
+  // Code goes here...
+};
---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Web3 from "web3";
+import Identicon from "identicon.js";
+import "./App.css";
+import Decentragram from "../abis/Decentragram.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
+
+const App = () => {
+  const [account, setAccount] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div>
+      <Navbar account={account} />
+      {loading ? (
+        <div id="loader" className="text-center mt-5">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <Main
+        // Code...
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
