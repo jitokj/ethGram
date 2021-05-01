41b3d8dcca3dcde724467c43ae10288debbe2d37
jito <jitokj3@gmail.com>
2021-05-01 16:13:55 +0530

    added web3 functions

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,17 @@ import Ethgram from "../abis/Ethgram.json";
 import Navbar from "./Navbar";
 import Main from "./Main";
 
+//Declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+
 const App = () => {
   const [account, setAccount] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [ethContract,setEthContract] = useState(null);
+  const [img,setImg] = useState([]);
+  const [imgCount,setImgCount] = useState(0);
+  const [buff,setBuff] = useState(null);
 
  useEffect(()=>{
     const  loadWeb3 = async ()=>{
@@ -29,22 +37,63 @@ const App = () => {
 
   useLayoutEffect(()=>{
     const loadBlockchainData = async()=>{
+      const ethereum = window.ethereum;
+      if(ethereum){
       const web3 = new Web3(window.ethereum);
+    
       const accounts = await web3.eth.getAccounts();
       setAccount(accounts[0]);
       const networkId = await web3.eth.net.getId();
       const networkData = Ethgram.networks[networkId];
       if(networkData){
         const ethgram = new web3.eth.Contract(Ethgram.abi,networkData.address);
+        setEthContract(ethgram);
+        const imagesCount = await ethgram.methods.imageCount().call();
+        setImgCount(imagesCount);
+        for(let i=1;i<=imagesCount;i++){
+          const image = await ethgram.methods.images(i).call();
+         
+          setImg(prevState=>{
+            return [...prevState,image]
+          })
+        }
+        setLoading(false);
       }
       else {
           window.alert('Ethgram contract not deployed to detected network');
       }
+    }
       
     }
 
     loadBlockchainData();
   },[])
+
+
+  const captureFile = (event)=>{
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = ()=>{
+      setBuff( Buffer(reader.result));
+    }
+  }
+
+  const uploadImage = (description)=>{
+    console.log("submitting file to the ipfs");
+    ipfs.add(buff,(error,result)=>{
+      console.log("ipfs result",result);
+      if(error){
+        console.error(error);
+        return;
+      }
+      setLoading(true);
+      ethContract.methods.uploadImage(result[0].hash,description).send({from:account}).on('transactionHash',(hash)=>{
+        setLoading(false);
+      })
+    })
+  }
 
   return (
     <div>
@@ -54,7 +103,7 @@ const App = () => {
           <p>Loading...</p>
         </div>
       ) : (
-        <Main
+        <Main captureFile={captureFile} uploadImage={uploadImage} images={img}
         // Code...
         />
       )}
